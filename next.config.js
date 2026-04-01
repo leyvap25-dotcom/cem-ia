b62b876136067caa1d070b837e022c838189ab93
leyvap25-dotcom <leyvap25@gmail.com>
2026-03-31 20:45:07 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BUILD_DATE: new Date().toISOString(),
+  },
+};
+module.exports = nextConfig;
